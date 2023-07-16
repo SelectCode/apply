e19b45faab0f0bbca56526177934ed3bb1a1aa80
Florian Baader <florian.baader@selectcode.de>
2023-07-16 11:16:17 +0200

    fix sitemap generation

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://example.com',
+  siteUrl: 'https://apply.selectcode.de',
   generateRobotsTxt: true, // (optional)
 }
