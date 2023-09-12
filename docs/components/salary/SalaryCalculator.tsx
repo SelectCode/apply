88462b3f27e9cf3de608f632fcf10e6d85d96404
Stephan Le <stephan.le@selectcode.de>
2023-09-12 09:52:07 +0200

    Refactor slider to additional component

--- docs/components/salary/SalaryCalculator.tsx
+++ docs/components/salary/SalaryCalculator.tsx
@@ -6,6 +6,7 @@ import {
     responsibilityComponent,
     SalaryComponent
 } from "@components/salary/SalaryModel";
+import {Slider} from "@components/salary/Slider";
 
 export default function SalaryCalculator() {
     const [hours, setHours] = useState(12);
@@ -58,14 +59,6 @@ export default function SalaryCalculator() {
                 value={experience}
                 onChange={setExperience}
                 outcome={`+${experience.toFixed(2)}€`}
-                descriptions={[
-                    "No practical experience",
-                    "Various university projects or personal projects, no team",
-                    "Several personal projects/team experience, multiple programming languages",
-                    "Full-stack development or language expert, several years of experience",
-                    "Fluent in several languages, understanding of architecture and systems",
-                    "Implementation of complete enterprise architectures",
-                ]}
             />
             <Slider
                 salaryComponent={knowledgeComponent}
@@ -75,14 +68,6 @@ export default function SalaryCalculator() {
                 value={knowledge}
                 onChange={setKnowledge}
                 outcome={`+${knowledge.toFixed(2)}€`}
-                descriptions={[
-                    "No specialized knowledge",
-                    "Introductory lectures, information from high school diploma",
-                    "Completed Bachelor's degree, all-rounder",
-                    "Expert in some complex topics, reads blogs and continues education",
-                    "Level of a Master's graduate, reads blogs and continues education",
-                    "Expert knowledge in several areas such as App, Frontend, Backend, Cloud, Hardware - understands how the entire IT world is connected and can understand very complex correlations and find errors",
-                ]}
             />
             <Slider
                 salaryComponent={responsibilityComponent}
@@ -92,14 +77,6 @@ export default function SalaryCalculator() {
                 value={responsibility}
                 onChange={setResponsibility}
                 outcome={`+${responsibilityOutCome.toFixed(2)}€`}
-                descriptions={[
-                    "No responsibility",
-                    "Technical Expert, Code Reviews, Architecture Decisions",
-                    "Leads small client project (up to 15k annual turnover)",
-                    "Leads medium client project (up to 100k annual turnover)",
-                    "Leads large client project (up to 500k annual turnover)",
-                    "Leads the entire company (up to 1 Mio annual turnover)",
-                ]}
             />
             <div className="">
                 <h2 className="text-center font-black text-xl">Your Salary:</h2>
@@ -110,62 +87,6 @@ export default function SalaryCalculator() {
                     <span>= {monthlySalary.toFixed(2)} Euro / Month</span>
                 </div>
             </div>
-        </div>
-    );
-}
-
-function Slider({
-                    salaryComponent,
-                    min,
-                    max,
-                    step,
-                    value,
-                    onChange,
-                    outcome,
-                    descriptions,
-                }: {
-    salaryComponent: SalaryComponent;
-    min: number;
-    max: number;
-    step: number;
-    value: number;
-    onChange: (value: number) => void;
-    outcome: string;
-    descriptions?: string[];
-}) {
-    return (
-        <div className="my-10">
-            <h2 className="flex flex-row justify-between">
-        <span className="text-xl font-bold">
-          {salaryComponent.title} · {value}
-        </span>
-                <span className="text-right text-lg text-success font-bold">
-          {outcome}
-        </span>
-            </h2>
-            <div className="my-3">
-                <input
-                    type="range"
-                    min={min}
-                    max={max}
-                    value={value}
-                    onChange={(e) => onChange(parseInt(e.target.value))}
-                    className="range"
-                    step={step}
-                />
-                <div className="w-full flex justify-between text-xs px-2">
-                    {new Array(max - min + 1).fill(0).map((_, i) => (
-                        <span>|</span>
-                    ))}
-                </div>
-            </div>
-            {descriptions && (
-                <div className="text-left">
-                    <ul>
-                        {salaryComponent.levels[value - min].criteria.map((it) => (<li>{it}</li>))}
-                    </ul>
-                </div>
-            )}
         </div>
     );
 }
